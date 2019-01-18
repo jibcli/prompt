1bf606f056a1912a42d471d513f7b4265cf5c708
Matt Vander Vliet <matthew.vander.vliet@oracle.com>
2019-01-18 10:44:21 -0700

    Added support for custom prompt adapters

--- src/plugin.ts
+++ src/plugin.ts
@@ -4,14 +4,43 @@ import { Provide } from '@jib/cli';
 
 export interface JibPrompt extends inquirer.Inquirer { }
 
+export interface IPromptAdapter {
+  prompt(questions: inquirer.Questions): Promise<any>;
+  prompt<T>(questions: inquirer.Questions<T>): Promise<T>;
+  prompt<T>(questions: inquirer.Questions<T>, cb: (answers: T) => any): inquirer.ui.Prompt;
+}
+
 @Provide<JibPrompt>({
   factory: () => JibPrompt.init(),
 })
-export class JibPrompt {
+export class JibPrompt implements IPromptAdapter {
+
+  /**
+   * plugin factory initializer
+   */
   public static init(): JibPrompt {
     const instance = new this();
+    const { _adapter } = this;
     return new Proxy(instance, {
       get: (target, prop) => target[prop] || inquirer[prop],
     });
+  }
+
+  /**
+   * Use custom prompt adapter to resolve questions into answers.
+   * @param adapter
+   */
+  public static adapter(adapter: IPromptAdapter): void {
+    this._adapter = adapter;
+  }
+
+  private static _adapter: IPromptAdapter = inquirer;
+
+  public prompt(questions: inquirer.Questions): Promise<any>;
+  public prompt<T>(questions: inquirer.Questions<T>): Promise<T>;
+  public prompt<T>(questions: inquirer.Questions<T>, cb: (answers: T) => any): inquirer.ui.Prompt;
+  public prompt<T>(questions: inquirer.Questions<T>, cb?: (answers: T) => any): Promise<T> | inquirer.ui.Prompt {
+    const { _adapter } = this.constructor as typeof JibPrompt;
+    return _adapter.prompt(questions, cb);
   }
 }
